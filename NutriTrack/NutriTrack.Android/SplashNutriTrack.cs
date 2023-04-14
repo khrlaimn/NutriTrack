1493fe1f8c4b935090998e92ef4dd3fd2659880b
aiman2001 <khrlaimn10@gmail.com>
2023-04-14 17:45:43 +0800

    Add project files.

---
+++ NutriTrack/NutriTrack.Android/SplashNutriTrack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS; 
+
+using static Android.Net.LocalSocketAddress;
+using NutriTrack.Droid;
+
+namespace NutriTrack.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/Splash", MainLauncher = true)]
+    public class SplashNutriTrack : Activity
+    {
+        public SplashNutriTrack()
+        {
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Window.DecorView.SetBackgroundColor(Android.Graphics.Color.White);
+            Finish();
+        }
+
+
+    }
+}
